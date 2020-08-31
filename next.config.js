9e4141be47887a3199129515eb56dcf08d63da66
Yukai Li <yukaili.geek@gmail.com>
2020-08-31 03:58:48 -0600

    I think I fixed the externals thing for real this time

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@ module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
-    config.externals = config.externals || {}
     if (isServer) {
-        config.externals['../../pkg/tideman_lib'] = `commonjs2 ${require('path').join(__dirname, 'pkg/tideman_lib.js')}`
+        config.externals = config.externals || []
+        config.externals.unshift({
+          '../../pkg/tideman_lib': `commonjs2 ${require('path').join(__dirname, 'pkg/tideman_lib.js')}`
+        })
     }
 
     // Important: return the modified config
