829568723490b78c2dce7c555073365b4b33ef51
Yukai Li <yukaili.geek@gmail.com>
2020-08-31 04:10:32 -0600

    Add proper GET check

--- pages/api/tideman.js
+++ pages/api/tideman.js
@@ -1,11 +1,19 @@
 import { find_winner } from '../../pkg/tideman_lib'
 
 export default (req, res) => {
+  if (req.method === 'GET') {
+    res.statusCode = 405
+    res.end()
+    return
+  }
+
   if (req.body) {
     res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
     res.send(find_winner(JSON.stringify(req.body)))
   } else {
     res.statusCode = 400
+    res.setHeader('Content-Type', 'application/json')
     res.send(JSON.stringify({'error': 'no body'}))
   }
 }
