c735a9c53b817454256baec14561975711e0ba40
Yukai Li <yukaili.geek@gmail.com>
2020-08-30 22:58:51 -0600

    Add ballots

---
+++ components/VotingCard.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import { Card, ListGroup, Button, ButtonGroup, Col } from 'react-bootstrap'
+import { FaChevronCircleUp, FaChevronCircleDown, FaVoteYea, FaPlusCircle } from 'react-icons/fa'
+import _ from 'lodash'
+
+function VotingItem(props) {
+  const { name, committed, canMoveUp, canMoveDown, onUpClick, onDownClick } = props
+
+  return (
+    <ListGroup.Item>
+      {!committed &&
+        <ButtonGroup>
+          <Button variant='secondary' label='Move up'
+            disabled={!canMoveUp} onClick={onUpClick}
+          >
+            <FaChevronCircleUp/>
+          </Button>
+          <Button variant='secondary' label='Move down'
+            disabled={!canMoveDown} onClick={onDownClick}
+          >
+            <FaChevronCircleDown/>
+          </Button>
+        </ButtonGroup>
+      }
+      &nbsp;&nbsp;{name}
+    </ListGroup.Item>
+  )
+}
+
+export default class VotingCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      candidateOrder: props.isEmpty ? [] : _.range(props.candidates.length),
+      committed: false,
+    }
+  }
+
+  onMoveDown = orderIndex => () => {
+    const newCandidateOrder = [...this.state.candidateOrder]
+    const removed = newCandidateOrder.splice(orderIndex, 1)
+    newCandidateOrder.splice(orderIndex + 1, 0, ...removed)
+    this.setState({candidateOrder: newCandidateOrder})
+  }
+
+  onMoveUp = orderIndex => () => {
+    const newCandidateOrder = [...this.state.candidateOrder]
+    const removed = newCandidateOrder.splice(orderIndex, 1)
+    newCandidateOrder.splice(orderIndex - 1, 0, ...removed)
+    this.setState({candidateOrder: newCandidateOrder})
+  }
+
+  onVoteClicked = () => {
+    this.setState({committed: true})
+    this.props.onVote()
+  }
+
+  onAddBallotClicked = () => {
+    this.props.onAddBallot()
+  }
+
+  render() {
+    const { isEmpty, candidates } = this.props
+    const numCandidates = candidates && candidates.length
+
+    return (
+      <Col className='mb-4'>
+        <Card className='h-100'>
+          <Card.Header>
+            Ballot
+          </Card.Header>
+          {!isEmpty &&                 
+            <ListGroup variant='flush'>
+              {this.state.candidateOrder.map((v, i) => {
+                return <VotingItem
+                  key={v}
+                  name={candidates[v].name}
+                  committed={this.state.committed}
+                  canMoveUp={i > 0}
+                  canMoveDown={i < numCandidates - 1}
+                  onUpClick={this.onMoveUp(i)}
+                  onDownClick={this.onMoveDown(i)}
+                />
+              })}
+            </ListGroup>
+          }
+          <Card.Body>
+            {isEmpty ? (
+              <Button block onClick={this.onAddBallotClicked}>
+                <FaPlusCircle/> Add ballot
+              </Button>
+            ) : (
+              <Button block disabled={this.state.committed}
+                onClick={this.onVoteClicked}
+              >
+                <FaVoteYea/> Vote{this.state.committed && ' cast'}
+              </Button>
+            )}
+          </Card.Body>
+        </Card>
+      </Col>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import styles from '../styles/Home.module.css'
 import React from 'react'
+import { Container, Row, Button, Col } from 'react-bootstrap'
+import { FaVoteYea } from 'react-icons/fa'
+
 import Header from '../components/Header'
-import { Container } from 'react-bootstrap'
 import StartModal from '../components/StartModal'
+import VotingCard from '../components/VotingCard'
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -10,7 +12,9 @@ export default class Home extends React.Component {
     this.state = {
       firstRun: true,
       showNewElectionModal: true,
-      candidates: []
+      candidates: [],
+      ballots: [],
+      pollClosed: false
     }
   }
 
@@ -24,7 +28,9 @@ export default class Home extends React.Component {
     this.setState({
       candidates: newCandidates,
       firstRun: false,
-      showNewElectionModal: false
+      showNewElectionModal: false,
+      ballots: [],
+      pollClosed: false
     })
   }
 
@@ -36,22 +42,70 @@ export default class Home extends React.Component {
     this.setState({showNewElectionModal: true})
   }
 
+  onVote = (i) => (order) => {
+    const newBallots = [...this.state.ballots]
+    newBallots[i] = {
+      committed: true,
+      order
+    }
+    this.setState({ballots: newBallots})
+  }
+
+  onAddBallot = () => {
+    const newBallots = [...this.state.ballots]
+    newBallots.push({
+      committed: false,
+      order: []
+    })
+    this.setState({ballots: newBallots})
+  }
+
+  onClosePoll = () => {
+    this.setState({pollClosed: true})
+  }
+
   render() {
+    const canClosePoll = this.state.ballots.length !== 0 &&
+      !this.state.ballots.some(b => !b.committed)
+
     return (
-      <Container>
+      <>
         <Header onNewElectionClick={this.onNewElectionButtonClick}/>
-        
-        <main className={styles.main}>
-
+        <main>
+          <Container>
+            <Row className='mt-3' xs={2} sm={3}>
+              {this.state.ballots.map((v, i) => {
+                return <VotingCard
+                  key={i}
+                  candidates={this.state.candidates}
+                  onVote={this.onVote(i)}
+                />
+              })}
+              {!this.state.pollClosed &&
+                <VotingCard isEmpty onAddBallot={this.onAddBallot} />
+              }
+            </Row>
+            <Row className='mb-3'>
+              <Button block onClick={this.onClosePoll} disabled={!canClosePoll}>
+                <FaVoteYea/> Close poll and show winner
+              </Button>
+            </Row>
+            {!canClosePoll &&
+              <Row className='mb-3'>
+                <Col>
+                  <div className='text-center'>To close poll, cast all ballots.</div>
+                </Col>
+              </Row>
+            }
+          </Container>
+          <StartModal
+            show={this.state.showNewElectionModal}
+            canCancel={!this.state.firstRun}
+            onSubmit={this.onSubmitNewElection}
+            onCancel={this.onCancelNewElection}
+          />
         </main>
-
-        <StartModal
-          show={this.state.showNewElectionModal}
-          canCancel={!this.state.firstRun}
-          onSubmit={this.onSubmitNewElection}
-          onCancel={this.onCancelNewElection}
-        />
-      </Container>
+      </>
     )
   }
 }
