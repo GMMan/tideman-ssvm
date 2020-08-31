273a8dd7ccbd14515c875defdf58aadfb9dfddd2
Yukai Li <yukaili.geek@gmail.com>
2020-08-31 04:03:13 -0600

    Add check for empty body (GET)

--- pages/api/tideman.js
+++ pages/api/tideman.js
@@ -1,6 +1,11 @@
 import { find_winner } from '../../pkg/tideman_lib'
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.send(find_winner(JSON.stringify(req.body)))
+  if (req.body) {
+    res.statusCode = 200
+    res.send(find_winner(JSON.stringify(req.body)))
+  } else {
+    res.statusCode = 400
+    res.send(JSON.stringify({'error': 'no body'}))
+  }
 }
